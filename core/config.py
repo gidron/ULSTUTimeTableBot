5cf949f7bdad95749831591e020225d2f5ff9505
Anton ill <anton.illenberger@qualixdev.com>
2026-03-09 13:37:34 +0400

    changes

--- core/config.py
+++ core/config.py
@@ -2,12 +2,6 @@ import socket
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ENV = ".env"
-hostname = socket.gethostname()
-
-if hostname != "gidron-laptop":
-    ENV = ".env.prod"
-
 
 class Settings(BaseSettings):
     bot_token: str
@@ -40,7 +34,7 @@ class Settings(BaseSettings):
     redis_port: int
 
     model_config = SettingsConfigDict(
-        env_file=ENV,
+        env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
